4f66dc3d8554c3736ee91fa0dee6ab9fc32b03aa
PanayiotaK <44776906+PanayiotaK@users.noreply.github.com>
2019-01-17 07:45:08 +0200

    Add files via upload

--- classes.js.js
+++ classes.js.js
@@ -4,8 +4,8 @@ class Foods{
         this.y=y || 20;
         this.height=h || 10;
         this.width=w || 10;
-		this.shape=sh ||"circle";
-		this.colour=colour || "black";
+        this.shape=sh ||"circle";
+        this.colour=colour || "black";
     }
     setColour(colour){
         if(typeof colour=="string"){
@@ -15,7 +15,7 @@ class Foods{
         return this.colour;
     }
     setShape(shape){
-        if(typeof colour=="string"){
+        if(typeof shape=="string"){
             this.shape=shape;}
     }
     getShape(){
@@ -67,17 +67,22 @@ class Foods{
                 k. triangle(this.x, this.y,this.x+this.width,this.y, this.x,this.y+this.height);
             }
             else{
-                triangle(this.x, this.y,this.x+this.width,this.y, this.x,this.y+this.height);;}
+                triangle(this.x, this.y,this.x+this.width,this.y, this.x,this.y+this.height);}
            
         }
-	}
+    }
+    reset(new_x,new_y){
+        this.x=new_x;
+        this.y=new_y;
+        
+    }
 
 }
 
 class buttons{
     constructor(sh_strW,sh_strok,x,y,h,w,sh_r,sh_g,sh_b,tex_s,tex,Tx,Ty,strW,strok,Col_tex){
-        this.sh_strW=sh_strW 
-        this.sh_strok=sh_strok 
+        this.sh_strW=sh_strW ;
+        this.sh_strok=sh_strok ;
         this.x=x;
         this.y=y;
         this.h=h;
@@ -89,9 +94,9 @@ class buttons{
         this.tex=tex;
         this.Tx=Tx;
         this.Ty=Ty;        
-        this.strW=strW 
-        this.strok=strok 
-        this.Col_tex=Col_tex 
+        this.strW=strW; 
+        this.strok=strok ;
+        this.Col_tex=Col_tex; 
 
     }
 
@@ -125,36 +130,85 @@ class buttons{
 
 class Players extends Foods{
     constructor(x,y,h,w,sh,colour,sp){
-		super(x,y,h,w,sh,colour);
-		this.Startspeed=sp || 20;
-		this.speed=sp || 20; }
-		 
-	 getCalspeed(){
-		 return this.speed;
-	 }
-	 setCalSpeed(v){
+        super(x,y,h,w,sh,colour);
+        this.Startspeed=sp || 20;
+        this.speed=sp || 20; }		 
+    getCalspeed(){
+        return this.speed;
+    }
+    setCalSpeed(v){
         if(typeof v=="number"){
-		    this.speed=this.Startspeed+score /v;}   
-	 }
-	
-	 
-	move(val1,val2,val3,val4){
-		if(this.x>val1){
-			this.x-=1;
-		}
-		if(this.x<val2){
-			this.x+=1;
-		}
-		if(this.y>val3){
-			this.y-=1;
-		}
-		if(this.y<val4){
-			this.y+=1;
-		}
-	}
-	
+            this.speed=this.Startspeed+score /v;}   
+    }
+ 
+    move(val1,val2,val3,val4){
+        if(this.x>val1){
+            this.x-=1;
+        }
+        if(this.x<val2){
+            this.x+=1;
+        }
+        if(this.y>val3){
+            this.y-=1;
+        }
+        if(this.y<val4){
+            this.y+=1;
+        }
+    }
+    Reset_properties(NewX,NewY,NewColour){
+        this.x=NewX;
+        this.y=NewY;
+        this.colour=NewColour;
+        this.speed=this.Startspeed;
+
+
+    }
+
 	
 }
+
+class Text{
+    constructor(Text_strWeight,Text_col,TextSize,Text_Stroke,Text_,Text_x,Text_y){
+        this.Text_strWeight=Text_strWeight || 1;
+        this.Text_col=Text_col || 0;
+        this.TextSize=TextSize || 25;
+        this.Text_Stroke=Text_Stroke || 0;
+        this.Text=Text_;
+        this.Text_x=Text_x;
+        this.Text_y=Text_y;
+
+    }
+    draw(k){
+        if(k){
+            k.strokeWeight(this.Text_strWeight);
+            k.stroke(this.Text_Stroke);
+            k.fill(this.Text_col);           
+            k.textSize(this.TextSize);
+            k.text(this.Text,this.Text_x,this.Text_y);
+        }
+        else{
+            strokeWeight(this.Text_strWeight);
+            stroke(this.Text_Stroke);
+            fill(this.Text_col);           
+            textSize(this.TextSize);
+            text(this.Text,this.Text_x,this.Text_y);
+        }
+
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
 
 
 	
